ca60a14da535d6b1cd5cc0feba2f34f6245496fa
huxiaoshi <huxiaoshi145@icloud.com>
2024-12-13 20:30:10 +0800

    1.0R

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ##########################################################################################
 #
-# oneuifx_for_1+
+# oneuifx_for_AOSPA
 # by Bit
 #
 ##########################################################################################
@@ -45,10 +45,11 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "  oneui音效 for ColorOS on OnePlus 1.0    "
+  ui_print "  oneui音效 for 类原生系统(AOSPA)分支版"
   ui_print "              By Bit"
-  ui_print "音效包来自三星S21FE，系统版本oneui6.1"
+  ui_print "音效包来自三星Galaxy Note10+，系统版本oneui4.1"
   ui_print "更新内容详见发布时Release自述"
+  ui_print "注意事项:由于类原生系统默认铃声选取app不是soundpicker，而是音效app，所以你可能需要安装第三方铃声设置软件才能使用本模块中的所有来电、短信铃声"
   ui_print "请自备救砖模块，死了不关我事"
   ui_print "*******************************"
 }
@@ -84,7 +85,7 @@ REPLACE="
 #释放文件，普通shell命令
 on_install() {
   ui_print "释放文件……"
-  ui_print "再次提示！不...不备救砖...死...刷......刷死...活该喵！！！"
+  ui_print "再次提示！不备救砖，刷死活该！"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 }
 
