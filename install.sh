ac7c051ba4b7aa6f0905c30f5923b01fa6adcdad
fuydg <194613759+fuydg@users.noreply.github.com>
2025-04-08 22:36:35 +0800

    chore: 我才是Zako

--- install.sh
+++ install.sh
@@ -1,81 +1,121 @@
 ##########################################################################################
 #
-# oneuifx - by Bit
+# oneuifx
+# by Bit
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
 #
 ##########################################################################################
 
 ##########################################################################################
-# 使用说明 / Instructions:
-#
-# 1. 将要替换的文件放入 system 目录（删除占位文件）
-# 2. 在 module.prop 中填写模块信息
-# 3. 配置本文件（config.sh）
-# 4. 如需开机脚本，可添加到 common/post-fs-data.sh 或 common/service.sh 中
-# 5. 如需添加系统属性，可修改 common/system.prop
+# Configs
 ##########################################################################################
 
-##########################################################################################
-# 配置项 / Configs
-##########################################################################################
-
-# 是否启用 Magic Mount（大多数模块需要）
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
 SKIPMOUNT=false
+#是否安装模块后自动关闭，改为true，安装后不会自动勾选启用
 
-# 是否加载 common/system.prop
+# Set to true if you need to load system.prop
 PROPFILE=false
+#是否使用common/system.prop文件
 
-# 是否启用 post-fs-data 脚本
+# Set to true if you need post-fs-data script
 POSTFSDATA=false
+#是否使用post-fs-data脚本执行文件
 
-# 是否启用 late_start service 脚本
+# Set to true if you need late_start service script
 LATESTARTSERVICE=false
+#是否在开机时候允许允许common/service.sh中脚本
 
 ##########################################################################################
-# 安装信息 / Installation Message
+# Installation Message
 ##########################################################################################
+
+# Set what you want to show when installing your mod
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "  oneui音效 for HyperOS 1.8"
+  ui_print "  oneui音效 for Hyperos 1.8    "
   ui_print "  By Bit"
-  ui_print "注意：本版本更换 SFX 源设备为 S21FE，旧版本不再维护"
-  ui_print "音效包源自三星 S21FE，系统版本 OneUI 6.1"
-  ui_print "更新内容详见发布页 Release"
-  ui_print "请自备救砖模块，刷机风险自负"
-  ui_print "注意：本版本开机动画适用于 Note12T 和 Poco F5，"
-  ui_print "      其他机型可能出现分辨率问题"
-  ui_print "若显示异常，请进入以下目录："
-  ui_print "/data/adb/modules/oneui_fx/system/product/media"
-  ui_print "将 bootanimation.zip.bak 重命名为 bootanimation.zip，并删除原文件"
+  ui_print "注意，本版本开始将更换SFX源设备为S21FE，旧版本将不再维护"
+  ui_print "音效包来自三星S21FE，系统版本oneui6.1"
+  ui_print "更新内容详见发布时Release自述"
+  ui_print "请自备救砖模块，死了不关我事"
+  ui_print "注意:这个版本的开机动画是针对于Note12T和Poco F5所作,其他机型使用,分辨率可能存在不一致情况，"
+  ui_print "如果你的手机显示开机动画异常，请尝试进入 /data/adb/modules/oneui_fx/system/product/media 目录下,将bootanimation.zip.bak重命名为bootanimation.zip，病删掉原来的"
   ui_print "*******************************"
 }
 
 ##########################################################################################
-# 替换列表 / Replace List
+# Replace list
 ##########################################################################################
 
-# 可填写需完整替换的系统路径，如：
-# /system/priv-app/SystemUI
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
+
+# This is an example
 REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
 "
 
-##########################################################################################
-# 安装过程 / Permissions
-##########################################################################################
+# Construct your own list here, it will override the example above
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
 
+"
+#添加您要精简的APP/文件夹目录
+#例如：精简状态栏，找到状态栏目录为  /system/priv-app/SystemUI/SystemUI.apk     
+#转化加入:/system/priv-app/SystemUI
+#（可以搭配高级设置获取APP目录）
+
+##########################################################################################
+# Permissions
+##########################################################################################
+#释放文件，普通shell命令
 on_install() {
-  ui_print "释放文件中……"
-  ui_print "再次提醒：请自备救砖手段，刷死不负责喵！"
+  ui_print "释放文件……"
+  ui_print "再次提示！不...不备救砖...死...刷......刷死...活该喵！！！"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 }
 
 set_permissions() {
-  # 默认权限配置，适用于大部分情况
-  set_perm_recursive $MODPATH 0 0 0755 0644
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Here are some examples for the set_perm functions:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+  #设置权限，基本不要去动
 }
 
 ##########################################################################################
-# 自定义函数区域 / Custom Functions
+# Custom Functions
 ##########################################################################################
 
-# 可在此处定义自定义函数，并在 update-binary 中调用
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
